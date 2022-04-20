cdaec753e27dc8bf95aba2e1693aa459c5d306e2
damij <damianojongerius@gmail.com>
2022-04-20 11:26:08 +0200

    speed limit // jumps / sprint
    dubble jump

--- Assets/Scripts/camera.cs
+++ Assets/Scripts/camera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class camera : MonoBehaviour
 {
-    public static float mouseSensitivity = 150f;
+    public static float mouseSensitivity = 500f;
 
     public Transform playerBody;
 
@@ -22,7 +22,7 @@ public class camera : MonoBehaviour
         float MouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
 
         xRotation -= MouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -40f, 40f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * MouseX);
--- Assets/Scripts/mov.cs
+++ Assets/Scripts/mov.cs
@@ -9,11 +9,18 @@ public class mov : MonoBehaviour
     public float Speed;
     public Rigidbody rb;
 
+    public float maxSpeed;
+    public float curMaxSpeed;
+    private bool walking = true ; 
+
+    public bool isGrounded;
+    public Vector3 jump;
+    public int jumps;
+
     public float gravity;
     public float jumpforce;
     bool grounded;
-    public float playerHeight;
-    public LayerMask whatisGround;
+    public float PlayerHeight;
 
     float horizontalInput;
     float verticalInput;
@@ -24,14 +31,35 @@ public class mov : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        Cursor.lockState = CursorLockMode.Locked;
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+        jumps = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f * 0.2f, whatisGround);
         PlayerInput();
+        PlayerSpeed();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded || Input.GetKeyDown(KeyCode.Space) && jumps > 0)
+        {
+
+            rb.AddForce(jump * jumpforce, ForceMode.Impulse);
+            isGrounded = false;
+            jumps--;
+        }
+
     }
 
     void PlayerInput()
@@ -43,11 +71,53 @@ public class mov : MonoBehaviour
     private void FixedUpdate()
     {
         MovePlayer();
+
+    }
+    public void PlayerSpeed()
+    {
+        if (rb.velocity.magnitude > curMaxSpeed)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, curMaxSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift)) 
+        {
+            walking = false;
+            curMaxSpeed = maxSpeed * 2;
+        }else
+        {
+            walking = true;
+            curMaxSpeed = maxSpeed;
+        }
     }
 
     public void MovePlayer()
     {
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        rb.AddForce(10f * Speed * moveDirection.normalized, ForceMode.Force);
+        if (!walking)
+        {
+           Speed = Speed * 2;
+        }
+        if (Input.GetKey(KeyCode.W) && isGrounded)
+        {
+            rb.AddForce(10f * Speed * moveDirection.normalized, ForceMode.Force);
+        }
+        else if (Input.GetKey(KeyCode.S) && isGrounded || Input.GetKey(KeyCode.D) && isGrounded || Input.GetKey(KeyCode.A) && isGrounded)
+        {
+            rb.AddForce(10f * (Speed / 2) * moveDirection.normalized, ForceMode.Force);
+        }
+        else if(isGrounded)
+        {
+            rb.velocity = new Vector3(0, (rb.velocity.y) , 0);
+        }
+        if (!walking)
+        {
+            Speed = Speed / 2;
+        }
+
+
     }
+
+
+
 }
